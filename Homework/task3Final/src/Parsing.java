dbc50daaf107a2638a87d85d8c8ac8d113097b89
alexsp63 <58887947+alexsp63@users.noreply.github.com>
2020-12-14 12:42:39 +0300

    Update Parsing.java

--- Homework/task3Final/src/Parsing.java
+++ Homework/task3Final/src/Parsing.java
@@ -2,8 +2,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Parsing {
+
+    public static Date yesterday() {
+        //потому что он возвращает json по вчерашней дате
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        return cal.getTime();
+    }
+
+    public static Date tomorrow(int n) {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, n);
+        return cal.getTime();
+    }
+
     public static String answer(String url) {
         System.out.println();
         System.out.println((char) 27 + "[36mPlease wait..." + (char)27 + "[0m");
