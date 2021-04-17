c6ff5f60486d56696cfc017979b00c59cf6bda1a
alexsp63 <58887947+alexsp63@users.noreply.github.com>
2021-04-18 01:04:43 +0300

    Delete project directory

--- project/src/main/java/com/popovaproject/project/repository/StatusTaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.popovaproject.project.repository;
-
-import com.popovaproject.project.entity.StatusTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusTaskRepository extends JpaRepository<StatusTask, Long> {
-}
